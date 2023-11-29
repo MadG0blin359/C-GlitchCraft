aaa2d1c8ca094048ce3c5f758295f639ffa0ffba
MadG0blin359 <shawaizshahid312+github@gmail.com>
2023-11-29 16:32:49 +0500

    Simple Arithmetic Problems

--- Practice_Problems/Simple_Arithmetics/runascii.c
+++ Practice_Problems/Simple_Arithmetics/runascii.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 
 int main() {
-	char letter1=97;
-	char letter2='d';
+	char letter1=97;	// Initializing 'letter1' with ASCII value of 'a'
+	char letter2='d';	// Initializing 'letter2' with character 'd'
 	
-	letter1=letter1+23;
-	letter2=letter2+22;
+	letter1=letter1+23;		// Adding 23 to ASCII value of 'a' to make it letter x
+	letter2=letter2+22;		// Adding 22 to ASCII value of 'd' to make it letter z
 	
+	// Printing the modified characters
 	printf("letter1 is %c & letter2 %c \n", letter1, letter2);
-	return 0; }
+
+	// Return 0 to indicate successful program execution
+	return 0; 
+	
+	}
