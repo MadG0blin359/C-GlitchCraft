107eaa87400e4a8a8d0ed371ae946a8cea458f4b
MadG0blin359 <shawaizshahid312+github@gmail.com>
2023-12-01 17:45:49 +0500

    Simple Arithmetic Problems

--- Practice_Problems/Simple_Arithmetics/runave.c
+++ Practice_Problems/Simple_Arithmetics/runave.c
@@ -1,16 +1,23 @@
 #include<stdio.h>
 
 int main() {
+	// Variables to store user input
 	int distance;
 	float litters;
 	
+	// Prompt the user to enter distance travelled
 	printf("Insert the distance travelled in km = ");
-	scanf("%d", &distance);
+	scanf("%d", &distance);		// Read & store the user input
 	
+	// Prompt the user to enter fuel in litters
 	printf("Insert the fuel spent in litters = ");
-	scanf("%f", &litters);
+	scanf("%f", &litters);		// Read & store the user input
 	
+	// Formula to calculate bike's average fuel consumption
 	float average = distance/litters;	
+	// Display the calculated result
 	printf("The bike\'s average consumption is %.2f (km/lt)\n", average);
 	
-	return 0; }
+	return 0; 
+	
+}
