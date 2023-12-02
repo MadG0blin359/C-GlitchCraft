1533e7f827bf6efb01597d6b646162bd0187b70e
MadG0blin359 <shawaizshahid312+github@gmail.com>
2023-12-02 10:15:54 +0500

    Simple Arithmetic Problems

--- Practice_Problems/Simple_Arithmetics/runC2F.c
+++ Practice_Problems/Simple_Arithmetics/runC2F.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 int main() {
+	// Variables to store the performend calculations for converting Celsius to Fahrenheit
 	float C, F;
 	
-	printf("The temperature input = ");
+	printf("\n\t-----Celsius to Fahrenheit Converter-----\n");
+
+	// Prompt the user to input celsius
+	printf("\nTemperature in celsius = ");
 	scanf("%f", &C);
 	
+	// Formula to perform the conversion
 	F = (9.0/5.0)*C+32;
 	
-	printf("Temperature output = %g\n", F);
+	printf("Temperature in Fahrenheit = %g\n", F);
 	return 0;
 }
-	
