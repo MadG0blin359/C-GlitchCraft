e0bf1b35e1b9265359135aa4f4da9c8a24428df3
Shawaiz Shahid <143931858+MadG0blin359@users.noreply.github.com>
2024-01-06 05:32:13 -0800

    Delete PracticeProblems/Simple_Arithmetics directory

--- PracticeProblems/Simple_Arithmetics/runhun.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main() {
-	int a=1090;
-	int hun;
-	hun=a%1000;
-	printf("%d",hun);
-	return 0; }
--- PracticeProblems/Simple_Arithmetics/runquo.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main() {
-	int a=18;
-	int b=2;
-	int div;
-	div=a/b;
-	printf("%d/%d = %d",a,b,div);
-	return 0; }
