45ff83abaf1cd347dc437a39158b0dff3eecbccf
MadG0blin359 <shawaizshahid312+github@gmail.com>
2025-06-20 14:42:20 +0500

    Code Stash

---
+++ PracticeProblems/Process Mangement/Fifo03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main() {
+	int fd1, n;
+	char buffer[100];
+
+	fd1 = open("fifo1", O_RDONLY);
+	n = read(fd1, buffer, 100);
+	printf("Reader Process %d has started.\n", getpid());
+	printf("Data received by receiver %d is: %s\n", getpid(), buffer);
+	return 0;
+}
