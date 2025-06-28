8f5302c7d75c6426b78a5a17ed12df7c66dcb69e
MadG0blin359 <shawaizshahid312+github@gmail.com>
2025-06-28 22:12:43 +0500

    Code Stash

---
+++ PracticeProblems/Process Mangement/shared_mem.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+    int shmid;
+    char *shm;
+
+    shmid = shmget((key_t)2345, 1024, 0751 | IPC_CREAT);
+    shm = shmat(shmid, NULL, 0);
+    
+    strcpy(shm, "Hello");
+    printf("Process 1: Wrote 'Hello' and waiting...\n");
+    sleep(2);
+
+    
+    strcpy(shm, " your name");
+    printf("Process 2: Wrote 'your name'\n");
+
+    
+    strcpy(shm, " rollnumber");
+    printf("Process 3: Wrote 'rollnumber'\n");
+
+    printf("Final Shared Memory Content: %s\n", shm);
+
+    return 0;
+}
+
